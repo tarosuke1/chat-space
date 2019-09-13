d94f0eefe835256da70e28cefa4d7345f931db76
tarosuke1 <tarosuket962@gmail.com>
2019-09-13 10:36:49 +0900

    user.group

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  root to: 'messages#index'
+  devise_for :users
+  root 'messages#index'
+  root 'groups#index'
+  resources :users, only: [:edit, :update, :index]
+  resources :groups, only: [:new, :create, :edit, :update] do
+    resources :messages, only: [:index, :create]
+  end
 end
